5aacea62b28d13abf98376c68d2528da2a3fe8cc
jnwiegers <67205118+jnwiegers@users.noreply.github.com>
2022-04-20 15:34:47 +0200

    added delft coords

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,8 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Delft, the Netherlands",
+    52.0151804,
+    4.3769027277268755 
   ],
   [
     "London, UK",
