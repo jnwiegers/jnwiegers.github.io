97f4de670654c25dffc8c6a189209a824602e13c
J. N. (Yannick) Wiegers <yannick.wiegers@gmail.com>
2022-05-24 13:48:52 +0200

    added terschelling

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,12 +1,17 @@
 var addressPoints = [
   [
-    "Delft, the Netherlands",
+    "Delft, the Netherlands - Talk",
     52.0151804,
     4.3769027
   ],
   [
-  "Ngounié Province, Gabon",
+  "Ngounié Province, Gabon - Fieldwork",
   -1.728286, 
    10.383191
-  ]
+  ],
+  [
+    "De Boschplaat, Terschelling - Fieldwork",
+    53.427947, 
+    5.467009
+    ]
 ];
